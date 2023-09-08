8d36ebbed34236e991139553717cdbe2690fedfb
abdulla <amgaamams@gmail.com>
2023-09-07 19:34:22 -0700

    All Done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,9 +11,7 @@
 
 int main(void)
 {
-	char quote[];
-
-	quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quote, 59);
 	return (1);
