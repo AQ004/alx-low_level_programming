ce1f17083e88b62ccabd4b351668e3d2fff2c196
abdulla <amgaamams@gmail.com>
2023-09-08 20:05:03 -0700

    Done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,14 +1,18 @@
 # include <stdio.h>
 
-/***/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 <=99; num1++)
+	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (num2 = num1; num2 <= 99;num2++)
+		for (num2 = num1; num2 <= 99; num2++)
 		{
 			if (num2 != num1)
 			{
