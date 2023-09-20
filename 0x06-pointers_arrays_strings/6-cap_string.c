321100016767b429a79a769e8ab16ffff57ba341
abdulla <amgaamams@gmail.com>
2023-09-20 10:21:41 -0700

    Done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,26 +10,20 @@
 char *cap_string(char *str)
 {
 	char *str1 = str;
-	int i, foundSep = 1;
-	char separators[] = " \t\n,;.!?\"(){}";
+	char separators[] = " \t\n,.!?\"(){}";
+	int i;
 
 	while (*str)
 	{
-
-		if (1)
-			for (i = 0; i < 13; i++)
-			{
-				if (*str == separators[i])
-					foundSep = 1;
-			}
-		else if (islower(*str) && foundSep)
+x:
+		for (i = 0; i < 12; i++)
 		{
-			*str -= 'a' - 'A';
-			foundSep = 0;
+			if (*str == separators[i] && islower(*(str + 1)))
+				*++str -= ('a' - 'A');
 		}
-		else
-			foundSep = 0;
 		str++;
+		if (*str != '\0')
+			goto x;
 	}
-	return (str1);
+		return (str1);
 }
