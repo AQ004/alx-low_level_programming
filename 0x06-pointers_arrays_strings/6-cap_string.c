40c2805d935235dc1df245fc4f51a3d33e247248
abdulla <amgaamams@gmail.com>
2023-09-20 08:59:10 -0700

    Done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,15 +1,5 @@
 #include "main.h"
-
-/**
- * _islower - function that takes
- * @c: eeeeefaf
- * Description: awefwfawer eif wieh wef
- * Return: aefrf
- */
-int _islower(char c)
-{
-	return (c >= 'a' && c <= 'z');
-}
+#include <ctype.h>
 
 /**
  * cap_string - function returns char array
@@ -28,7 +18,7 @@ char *cap_string(char *str)
 x:
 		for (i = 0; i < 13; i++)
 		{
-			if (*str == separators[i] && _islower(*(str + 1)))
+			if (*str == separators[i] && islower(*(str + 1)))
 				*++str -= ('a' - 'A');
 		}
 		str++;
