95de4d04b908faae20b100055fd176455f7dccf4
abdulla <amgaamams@gmail.com>
2023-09-21 01:22:32 -0700

    Done

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - function return pointer of characters
+ * @str: array of characters
+ *
+ * Description: function that encodes a string into 1337.
+ *
+ * Return: str1
+ */
+
+char *leet(char *str)
+{
+	char *str1 = str;
+	char letters[] = {'a', 'e', 'o', 't', 'l'};
+	int replaces[] = {4, 3, 0, 7, 1};
+	int i;
+
+	while (*str)
+	{
+		for (i = 0; i < sizeof(letters); i++)
+			if (*str == letters[i] ||
+					*str == letters[i] - 'a' + 'A')
+				*str == replaces[i] + '0';
+		str++;
+	}
+	return (str1);
+}
