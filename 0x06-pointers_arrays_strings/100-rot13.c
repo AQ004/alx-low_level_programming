828b1e0d6a8b44696aee0d935948d6d03a8d943a
abdulla <amgaamams@gmail.com>
2023-09-21 02:30:31 -0700

    Done

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - function that returns pointer to char
+ * @str: pointer to character
+ *
+ * Description: function that encodes a string using rot13
+ *
+ * Return: str1
+ */
+
+char *rot13(char *str)
+{
+	char *str1 = str;
+	char alphabetic[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i;
+
+	while (*str)
+	{
+		for (i = 0; i < 26 * 2; i++)
+			if (*str == alphabetic[i])
+				*str = rot13[i];
+		str++;
+	}
+	return (str1);
+}
