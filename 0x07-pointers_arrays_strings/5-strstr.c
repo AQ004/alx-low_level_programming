4d10b0f4baae2b1ae4438711c639770e9a6d6abc
abdulla <amgaamams@gmail.com>
2023-09-25 19:52:37 -0700

    Done

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * compareBet - int function
+ * @haystack: pointer to char
+ * @needle: pointer to char
+ * Return: 1 _ 0
+ */
+
+int compareBet(char *haystack, char *needle)
+{
+	/* if equal return 1 */
+	while (*haystack && *needle)
+	{
+		if (*haystack != *needle)
+			return (0);
+		haystack++;
+		needle++;
+	}
+	return (1);
+}
+
+/**
+ * _strstr - char pointer func
+ * @haystack: pointer to char
+ * @needle: pointer to char
+ * Return: haystack _ 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		if ((*haystack == *needle) && compareBet(haystack, needle)
+			return (haystack);
+		haystack++;
+	}
+	return (0);
+}
+
