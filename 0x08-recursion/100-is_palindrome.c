3cbd08fa4e04aa410e30bef5f9ab070abdf88fc1
abdulla <amgaamams@gmail.com>
2023-09-28 07:31:38 -0700

    Done

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * palindrome - function that returns 1 if a string is
+ *		a palindrome and 0 if not.
+ * @s: pointer to first char.
+ * @lastCh: pointer to last char.
+ *
+ * Return: if palindrome return (1), otherwise (0).
+ */
+
+int palindrome(char *s, char *lastCh)
+{
+	if (s == lastCh)
+		return (1);
+	if (*s != *lastCh)
+		return (0);
+	if (*s == *lastCh)
+		palindrome(s + 1, lastCh - 1);
+}
+
+/**
+ * is_palindrome - function that returns 1 if a string is
+ *              a palindrome and 0 if not.
+ * @s: char array.
+ *
+ * Return: palindrome function
+ */
+
+int is_palindrome(char *s)
+{
+	char *lastCh = s + strlen(s) - 1;
+
+	palindrome(s, lastCh);
+}
+
