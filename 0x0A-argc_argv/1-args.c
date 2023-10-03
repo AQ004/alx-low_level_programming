d33fc07f7996ddfaedc7b63c18aaedc319435c31
abdulla <amgaamams@gmail.com>
2023-10-02 20:05:36 -0700

    Done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,29 +9,10 @@
  * Return: 0 or 1 ..
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	if (argc == 2)
-	{
-		int i, le = 0, mon = atoi(argv[1]);
-		int cen[] = {25, 10, 5, 2, 1};
-
-		for (i = 0; i < 5; i++)
-		{
-			if (mon >= cen[i])
-			{
-				le += mon / cen[i];
-				mon %= cen[i];
-				if (mon % cen[i] == 0)
-				{
-					break;
-				}
-			}
-		}
-		printf("%d\n", le);
-	}
-	else
-		printf("Error\n"), return (1);
+	/* body */
+	printf("%d\n", argc - 1);
 	return (0);
 }
 
