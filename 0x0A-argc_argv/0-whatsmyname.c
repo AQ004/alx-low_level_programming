190b781b1a4ed18a0d1dba8089543ca1d2dafe1f
abdulla <amgaamams@gmail.com>
2023-10-02 19:46:04 -0700

    Done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: awd
+ * @argv: axd
+ * Return: Always 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	/*	body	*/
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
