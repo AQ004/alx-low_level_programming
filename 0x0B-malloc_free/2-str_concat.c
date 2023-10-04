61847db9c6d25e556a2d89bee4179cb786d87658
abdulla <amgaamams@gmail.com>
2023-10-04 09:45:03 -0700

    Done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,12 +11,12 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i;
-
 	char *ptr = (char *) malloc(strlen(s1) + strlen(s2) + 1);
+
 	if (ptr == NULL)
 		return (NULL);
 	strcpy(ptr, s1);
-	for(i = strlen(s1); i <= strlen(s1) + strlen(s2); i++)
+	for (i = strlen(s1); i <= strlen(s1) + strlen(s2); i++)
 		ptr[i] = s2[i - strlen(s1)];
 	return (ptr);
 }
