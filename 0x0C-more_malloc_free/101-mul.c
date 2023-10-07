fbdb1dc91b1411484976a515c8c4e5257ed560be
abdulla <amgaamams@gmail.com>
2023-10-07 15:22:25 -0700

    Done

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,17 +2,29 @@
 #include <string.h>
 /**
  * main - Entry point
+ * @argc: number of arguments.
+ * @argv: arguments.
  *
- * Return: 
+ * Return: exit(98)__printf
  */
 
-int main()
+int main(int argc, char *argv[])
 {
+	int i, j;
+
 	if (argc != 3)
-		printf("Error\n"), exit(98);
-	if (isdigit(atoi(argv[1])) && isdiggit(atoi(argv[2])))
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	else
-	printf("Error\n"), exit(98);
+	{
+		printf("Error\n")
+		exit(98);
+	}
+
+	for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j] != '\0'; j++)
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 }
 
