49c827c9c415cce51aaddc12eacf48ef4143700e
abdulla <amgaamams@gmail.com>
2023-10-07 15:01:29 -0700

    Done

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * main - Entry point
  *
@@ -8,5 +8,11 @@
 
 int main()
 {
+	if (argc != 3)
+		printf("Error\n"), exit(98);
+	if (isdigit(atoi(argv[1])) && isdiggit(atoi(argv[2])))
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+	printf("Error\n"), exit(98);
 }
 
