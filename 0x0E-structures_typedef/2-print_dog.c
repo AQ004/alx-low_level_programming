5726b799d215a9d8bc89d27003b1fde50449be72
abdulla <amgaamams@gmail.com>
2023-10-11 07:07:40 -0700

    Done

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * print_dog - function that prints a struct dog
+ * @d: Struct Dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		print("Name: %s\nAge: %f\nOwner: %s", d->name ? d->name : "(nul)", (*p).age,
+				(*d).owner ? d->owner: "(nul)");
+	}
+}
+
