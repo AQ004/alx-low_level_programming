bf98e5b4257db1bffa42fa6698de7b143ac12f60
abdulla <amgaamams@gmail.com>
2023-10-12 10:00:51 -0700

    Done

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,86 @@
+#include "variadic_functions.h"
+
+void print_char(char *Separate, va_list x);
+void print_int(char *Separate, va_list x);
+void print_float(char *Separate, va_list x);
+void print_string(char *Separate, va_list x);
+
+/**
+ * print_all - function that prints anything
+ * @format: list of types of arguments passed to the function
+ * ...: args.
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i, j;
+	va_list x;
+	char *Separate = "";
+	datType_s FormChar[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL},
+	};
+
+	va_start(x, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (FormChar[j].datType)
+		{
+			if (format[i] == FormChar[j].datType[0])
+			{
+				FormChar[j].function(Separate, x);
+				Separate = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n"), va_end(x);
+}
+
+/**
+ * print_char - function to print character
+ * @separate: separator
+ * @x: va_list (arg. pointer)
+ */
+void print_char(char *Separate, va_list x)
+{
+	printf("%s%c", Separate, va_arg(x, int));
+}
+/**
+ * print_int - function to print integer.
+ * @separate: separator
+ * @x: va_list (arg. pointer)
+ */
+void print_int(char *Separate, va_list x)
+{
+	printf("%s%d", Separate, va_arg(x, int));
+}
+/**
+ * print_float - function to print float
+ * @separate: separator
+ * @x: va_list (arg. pointer)
+ */
+void print_float(char *Separate, va_list x)
+{
+	printf("%s%f", Separate, va_arg(x, double));
+}
+/**
+ * print_string - function to print string
+ * @separate: separator
+ * @x: va_list (arg. pointer)
+ */
+void print_string(char *Separate, va_list x)
+{
+	char *S = va_arg(x, char *);
+
+	if ((int)(!S))
+		S = "(nil)";
+	printf("%s%s", Separate, S);
+}
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,17 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct datType - specifiers and datatypes
+ * @datType: %s %i %f %c
+ * @function: function that makes that prints the specifier
+ */
+typedef struct datType
+{
+	char *datType;
+	void (*function)(char *, va_list);
+} datType_s;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
