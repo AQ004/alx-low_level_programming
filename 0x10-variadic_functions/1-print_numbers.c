6bfd391803c0a90ded95eb5281adb591b8988bee
abdulla <amgaamams@gmail.com>
2023-10-12 05:24:59 -0700

    Done

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers, \n
+ * @separator:
+ * @n:
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+}
+
