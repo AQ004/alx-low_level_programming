2ddab133dcd16850f92efcc76da69f45e4ff86d3
abdulla <amgaamams@gmail.com>
2023-10-11 18:05:59 -0700

    Done

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point.
+ * @argc: argument counter.
+ * @argv: argumrnt vector.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int (*function)(int, int);
+
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	function = get_op_func(argv[2]);
+	if (!function)
+		printf("Error\n"), exit(99);
+	printf("%d\n", op_func(a, b));
+	return (0);
+}
+
