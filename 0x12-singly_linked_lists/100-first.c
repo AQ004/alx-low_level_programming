14e6e0771d26867a31fe5a368fa33c8af176d064
abdulla <amgaamams@gmail.com>
2023-10-19 18:43:36 -0700

    Done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _function100 - constructor
+ */
+void _function100(void) __attribute__ ((constructor));
+void _function100(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
+
