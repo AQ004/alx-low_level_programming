3ebd6dc799afd16d88bbf544d31d51405ab5f266
abdulla <amgaamams@gmail.com>
2023-10-19 17:08:54 -0700

    Done

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,69 @@
+#include "lists.h"
+
+/**
+ * _strcpy - function that copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ * @dest: array of src pasted in this array
+ * @src: array of chars coppied from it to the another array
+ *
+ * Return: dest array
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+	int index = -1;
+
+	do {
+		index++;
+		dest[index] = src[index];
+	} while (src[index] != '\0');
+	return (dest);
+}
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: pointer to char
+ *
+ * Return: length of the array
+ */
+
+int _strlen(const char *s)
+{
+	int counter;
+
+	for (counter = 0; s[counter] != '\0'; counter++)
+		;
+	return (counter);
+}
+
+/**
+ * add_node - function that adds a new node
+ *		at the beginning of a list_t list.
+ * @head: list_t
+ * @str: string
+ *
+ * Return: new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+
+	newNode = malloc(sizeof(list_t));
+	if (newNode == 0)
+		return (NULL);
+	newNode->str = strdup(str);
+	/* newNode->str = malloc(strlen(str) + 1); */
+	if (newNode->str == 0)
+	{
+		free(newNode);
+		return (NULL);
+	}
+	/* strcpy(newNode->str, str); */
+	newNode->len = _strlen(str);
+	newNode->next = *head;
+	*head = newNode;
+	return (newNode);
+}
+
