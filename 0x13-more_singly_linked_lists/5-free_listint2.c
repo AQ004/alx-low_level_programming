aa7f08932186ce3f7b8a2e2116f3a84a8ea9b32f
abdulla <amgaamams@gmail.com>
2023-10-25 18:52:23 -0700

    Done

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,24 @@
 
 void free_listint2(listint_t **head)
 {
-	if (*head)
+	listint_t *tmp;
+
+	while (*head)
 	{
-		free_listint2(&(*head)->next);
-		free(*head);
+		tmp = *head;
+		*head = *head->next;
+		free(tmp);
 	}
 	*head = NULL;
+
+	/**
+	 * error: check:
+	 * if (*head)
+	 * {
+	 * free_listint2(&(*head)->next);
+	 * free(*head);
+	 * }
+	 * *head = NULL;
+	 */
 }
 
