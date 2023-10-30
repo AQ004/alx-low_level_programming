2642bca2803a0c537e7d6799aa7c199c89ce6756
abdulla <amgaamams@gmail.com>
2023-10-30 15:14:27 -0700

    Done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a txt file and prints it to the POSIX stdout.
+ * @filename:
+ * @letters:
+ *
+ * Return:
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+}
+
