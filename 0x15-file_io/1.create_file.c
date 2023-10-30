f0c99b1ff8afdbc3d0b1697613444788621dadbb
abdulla <amgaamams@gmail.com>
2023-10-30 16:59:23 -0700

    Done

--- 0x15-file_io/1.create_file.c
+++ 0x15-file_io/1.create_file.c
@@ -28,29 +28,18 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int file;
-	ssize_t n;
+	ssize_t n = 0;
+	ssize_t length = _strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
-
-	/* Open the file */
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	/* open the file */
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file == -1)
 		return (-1);
-
-	/* Write to the file */
-	if (!text_content)
-		n = write(file, text_content, _strlen(text_content));
-	else
-		n = 0;
-
-	/* Check if write was successful */
-	if (n == -1 || n != _strlen(text_content))
-		n = -1;
-
-	/* Close the file */
+	if (length)
+		n = write(file, text_content, length);
 	close(file);
-
-	return (n == -1 ? -1 : 1);
+	return (n == length ? 1 : -1);
 }
 
