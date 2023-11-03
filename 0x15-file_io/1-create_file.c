c6e96051dd17b6f0fba0bdd7622152187e56af52
abdulla <amgaamams@gmail.com>
2023-11-03 04:20:02 -0700

    Done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t n = 0;
 	ssize_t length = _strlen(text_content);
 
-	if (filename == NULL)
+	if (filename == NULL || text_content == NULL)
 		return (-1);
 	/* open the file */
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
