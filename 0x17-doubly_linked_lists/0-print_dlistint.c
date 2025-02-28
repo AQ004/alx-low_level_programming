af9cb6fb05e02b617c424d98c96e438563cc33a7
abdulla <amgaamams@gmail.com>
2025-02-28 11:35:33 +0200

    done

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - function that prints the values of the doubly linked lists
+ * @h: head of the DLL
+ * Description: function that prints all the elements of a dlistint_t list
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *t = h;
+	size_t counter = 0;
+
+	while (t != NULL)
+	{
+		print("%d\n", t->n);
+		t = t->next;
+		counter += 1;
+	}
+
+	return (counter);
+}
+
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,3 @@
+This is README file
+for the lesson of Doubly Linked_lists
+NULL = HEAD = .. = TAIL = NULL
